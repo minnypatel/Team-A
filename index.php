966aa078f692950215923e0286913941bd37aaee
Kirsty James <kirstyjames@Kirstys-MacBook-Air-2.local>
2017-04-01 13:00:17 +0100

    addition of basic nav bar

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
-<?php
+<html>
+    <head>
+        <title>Homepage</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="CSS/styles.css">
+    </head>
+    <body>
+        <?php include "Views/navbar.phtml"; ?> 
+    </body>
+</html>
 
-echo "HELLO WORLD!";
