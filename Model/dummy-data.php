933cd1812dc02f5c005cb79be8808814ec17aca2
Kirsty James <kirstyjames@Kirstys-MacBook-Air-2.local>
2017-04-01 17:37:09 +0100

    added articles template and styling

---
+++ Model/dummy-data.php
@@ -0,0 +1,27 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+$articles = ['Article 1' => [
+    'title' => 'Article 1', 
+    'image' => "Images/2002.jpg",
+    'body' => 'Whatever PBR&B master cleanse, next level direct trade food truck four loko heirloom pabst. Bushwick wayfarers green juice, typewriter banh mi gentrify poutine vexillologist salvia lyft lo-fi man bun. Letterpress waistcoat vice, small batch chambray man braid sartorial irony art party marfa tattooed migas pinterest. Taxidermy locavore snackwave, sartorial truffaut shabby chic prism. Photo booth dreamcatcher truffaut gentrify, blue bottle whatever gochujang pok pok bushwick. Dreamcatcher quinoa poutine glossier, meditation unicorn edison bulb bicycle rights master cleanse cronut. Post-ironic dreamcatcher slow-carb, pop-up asymmetrical chicharrones chambray literally chillwave pok pok food truck single-origin coffee vexillologist.
+                Pour-over fashion axe woke ennui, affogato chambray distillery kombucha forage. Succulents kickstarter chambray art party, scenester truffaut meh DIY etsy salvia lumbersexual migas retro literally put a bird on it. Banh mi ramps leggings pop-up, wolf fashion axe unicorn flexitarian bespoke fingerstache blue bottle bicycle rights tote bag. DIY vinyl fap, selvage cornhole shabby chic 8-bit swag mustache. Heirloom post-ironic pitchfork YOLO, hell of cornhole umami brunch selvage marfa hammock poke lomo mlkshk four loko. Jianbing drinking vinegar venmo art party. Cornhole street art gastropub intelligentsia, poutine seitan hella waistcoat swag ugh lumbersexual.
+                YOLO quinoa mumblecore, 3 wolf moon pop-up truffaut hoodie flannel taxidermy you probably havent heard of them. Paleo hot chicken quinoa, ethical lumbersexual try-hard cardigan hashtag. Jean shorts plaid post-ironic salvia tousled etsy. Disrupt pinterest lyft, plaid live-edge lumbersexual venmo poutine marfa try-hard chambray. Chambray YOLO whatever vegan tilde, humblebrag banjo authentic seitan polaroid thundercats. Subway tile godard distillery sriracha, hell of chillwave selfies sustainable banh mi whatever farm-to-table. XOXO cred woke, actually tousled narwhal tbh paleo asymmetrical helvetica direct trade retro butcher lumbersexual hexagon. Pop-up iceland everyday carry, fap plaid dreamcatcher lyft keytar stumptown hexagon etsy trust fund succulents. Sartorial poke sriracha ennui glossier, unicorn whatever. Post-ironic pug air plant single-origin coffee synth, irony hashtag copper mug. Hammock street art kinfolk artisan. Blue bottle bespoke letterpress, leggings sriracha food truck etsy waistcoat. Authentic everyday carry lyft chartreuse, bitters cred la croix godard +1 chambray. Raw denim succulents chartreuse, four loko poutine health goth godard chillwave farm-to-table tilde iceland aesthetic tote bag everyday carry cold-pressed.',
+    'contributor' => 'Jasmin'
+    ], 
+    
+['Article 2' => [
+    'title' => 'Article 2', 
+    'image' => '/Images/darjeeling2.png',
+    'body' => 'This is article 2 body.',
+    'contributor' => 'Kirsty'
+        ]
+    ]
+];
+
+
--- index.php
+++ index.php
@@ -4,9 +4,17 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="CSS/styles.css">
+        <link href="https://fonts.googleapis.com/css?family=Josefin+Slab" rel="stylesheet">
     </head>
     <body>
-        <?php include "Views/navbar.phtml"; ?> 
+        <?php include "Views/navbar.phtml"; ?>
+        <div class="container">
+        <div class="main">
+            <?php include "Views/article.phtml"; ?>
+            <?php include "Views/article.phtml"; ?>
+        </div>
+        </div>
+       
     </body>
 </html>
 
