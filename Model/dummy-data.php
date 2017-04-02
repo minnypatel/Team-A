919e33d0222e98ddc030b03dda4cb9a3b4856552
Chris <christopher.lee@sky.uk>
2017-04-02 22:03:48 +0100

    Fixing Dummy Data

--- Model/dummy-data.php
+++ Model/dummy-data.php
@@ -1,38 +1,34 @@
 <?php
 
 namespace Model\Dummydata;
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/*
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
 
 $articles = ['Article 1' => [
-<<<<<<< HEAD
-                'title' => 'blah blah blah', 
-=======
-                'title' => 'You probably haven\'t heard of Hipster Ipsum - Jasmin will tell you all about it', 
->>>>>>> 3691e11a810b2b82a1db5891cbde5ff2c69ab63b
-                'image' => "Images/2002.jpg",
-                'body' => 'Bespoke lo-fi succulents kombucha, bitters blog dreamcatcher farm-to-table la croix etsy cardigan intelligentsia leggings raw denim pickled. 3 wolf moon tousled cold-pressed, glossier fap knausgaard pitchfork activated charcoal 8-bit craft beer. Freegan pug you probably haven\'t heard of them master cleanse. Lo-fi affogato vexillologist flannel organic, franzen retro trust fund hella chia meditation iPhone. Literally twee 8-bit narwhal bicycle rights. Forage man braid lomo hell of, bicycle rights cred quinoa. Dreamcatcher ethical drinking vinegar, edison bulb disrupt photo booth offal cronut.',
-                'contributor' => 'Jasmin'
-                ], 
-            'Article 2' => [
-                'title' => 'Tattooed Kale Chips', 
-                'image' => '/Images/darjeeling2.png',
-                'body' => 'Leggings meggings wolf, austin tbh post-ironic drinking vinegar vaporware slow-carb listicle tattooed kale chips pour-over. Irony slow-carb messenger bag, yuccie hoodie pickled pop-up microdosing art party YOLO jean shorts polaroid. Messenger bag sustainable four dollar toast chia cold-pressed. Art party salvia chicharrones bicycle rights, locavore gastropub cold-pressed edison bulb knausgaard artisan blog la croix. Tbh synth cred lumbersexual, crucifix kitsch 90\'s tumeric bespoke hot chicken migas vexillologist mixtape. Four dollar toast pinterest meditation, tousled 8-bit viral literally lyft brooklyn four loko. Jianbing forage biodiesel you probably haven\'t heard of them pickled, paleo occupy tattooed plaid cold-pressed.',
-                'contributor' => 'Kirsty'
-                ]
-            ];
+               'title' => 'You probably haven\'t heard of Hipster Ipsum - Jasmin will tell you all about it',
+               'image' => "Images/2002.jpg",
+               'body' => 'Bespoke lo-fi succulents kombucha, bitters blog dreamcatcher farm-to-table la croix etsy cardigan intelligentsia leggings raw denim pickled. 3 wolf moon tousled cold-pressed, glossier fap knausgaard pitchfork activated charcoal 8-bit craft beer. Freegan pug you probably haven\'t heard of them master cleanse. Lo-fi affogato vexillologist flannel organic, franzen retro trust fund hella chia meditation iPhone. Literally twee 8-bit narwhal bicycle rights. Forage man braid lomo hell of, bicycle rights cred quinoa. Dreamcatcher ethical drinking vinegar, edison bulb disrupt photo booth offal cronut.',
+               'contributor' => 'Jasmin'
+               ],
+           'Article 2' => [
+               'title' => 'Tattooed Kale Chips',
+               'image' => '/Images/darjeeling2.png',
+               'body' => 'Leggings meggings wolf, austin tbh post-ironic drinking vinegar vaporware slow-carb listicle tattooed kale chips pour-over. Irony slow-carb messenger bag, yuccie hoodie pickled pop-up microdosing art party YOLO jean shorts polaroid. Messenger bag sustainable four dollar toast chia cold-pressed. Art party salvia chicharrones bicycle rights, locavore gastropub cold-pressed edison bulb knausgaard artisan blog la croix. Tbh synth cred lumbersexual, crucifix kitsch 90\'s tumeric bespoke hot chicken migas vexillologist mixtape. Four dollar toast pinterest meditation, tousled 8-bit viral literally lyft brooklyn four loko. Jianbing forage biodiesel you probably haven\'t heard of them pickled, paleo occupy tattooed plaid cold-pressed.',
+               'contributor' => 'Kirsty'
+               ]
+           ];
 
 
 /* Dummy data for login*/
 $users = [ 'amardeep' =>[
-    'username' => 'amardeep',
-    'password' => 'test'
+   'username' => 'amardeep',
+   'password' => 'test'
 ],
-    'chris' => [
-        'username' => 'chris',
-        'password' => 'test'
-        ]
-    ];
+   'chris' => [
+       'username' => 'chris',
+       'password' => 'test'
+       ]
+   ];
