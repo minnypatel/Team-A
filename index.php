98ff71229f0b45ac745cd3c56b63d5880a67549b
Chris <christopher.lee@sky.uk>
2017-04-03 14:47:13 +0100

    added proof of concept for a twitter feed

--- index.php
+++ index.php
@@ -30,6 +30,7 @@ echo $stmt;*/
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="CSS/styles.css">
         <link href="https://fonts.googleapis.com/css?family=Josefin+Slab" rel="stylesheet">
+         <script async src='JS/newjavascript.js'></script>
     </head>
     <body>
         <?php // include "Views/navbar.phtml"; ?>
@@ -38,9 +39,11 @@ echo $stmt;*/
         <div class="main">
             <?php echo display('article', ['articles' => $articles, 'articleID' => 'Article 1' ] ); ?>
             <?php echo display('article', ['articles' => $articles, 'articleID' => 'Article 2' ] ); ?>
+            <a class="twitter-timeline" data-width="300" data-height="500" data-theme="dark" data-link-color="#9266CC" href="https://twitter.com/SkySports">Tweets by SkySports</a>
         </div>
         </div>
        
+         <script src='https://platform.twitter.com/widgets.js' async></script>
     </body>
 </html>
 
