e6c59095edc219bddac9fec5a9aafdfab4eda3f9
Jasmin Patel <minnypatel@googlemail.com>
2017-04-04 21:37:59 +0100

    singleton class db connection cleaned up

--- Model/Dbconnection.php
+++ Model/Dbconnection.php
@@ -1,6 +1,6 @@
 <?php
 
-Class Dbconnection{
+Class Dbconnection {
     
     private static $instance = null; 
     private $connection; 
@@ -9,16 +9,12 @@ Class Dbconnection{
     private $username = "root";
     private $password = "";
     
-    private function __construct(){
-   
-    /*function getdbconnect(){*/
-        
+    private function __construct() {
         $this->connection = new PDO($this->servername, $this->username, $this->password);
         $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         echo "Connection successful"; 
         return $this->connection;
     }
-
 
     public static function getInstance() {
         if (self::$instance == null){
@@ -27,32 +23,10 @@ Class Dbconnection{
         return self::$instance;
     }
     
-    public function getConnection(){
+    public function getConnection() {
         return $this->connection; 
     }
 } 
  
-$instance = Dbconnection::getInstance();
-$connection = $instance->getConnection(); 
-
-
-
-
-/*Class Dbconnection{
-    
-    public $servername = "mysql:host=127.0.0.1;dbname=blogdb";
-    public $username = "root";
-    public $password = "";
-    
-    function getdbconnect(){
-        $connection = new PDO($this->servername, $this->username, $this->password);
-        $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        echo "Connected successfully"; 
-        return $connection;
-    }
-}
- */
-
-
 ?>
 
--- sandpit.php
+++ sandpit.php
@@ -1,21 +1,12 @@
 <?php
 
-
-include_once 'Controller/display.php';
-include_once 'Model/dummy-data.php';
 include_once 'Model/DbConnection.php';
 
-use function Controller\display;
-
-
-/* 
- $newConnection = new DbConnection(); 
-$newConnection = $newConnection->getDb(); // here you get the connection
- */
-
-
+$instance = Dbconnection::getInstance();
+$connection = $instance->getConnection(); 
+var_dump($connection);
     
-$stmt = $newConnection->prepare("SELECT id, title FROM article");
+$stmt = $connection->prepare("SELECT id, title FROM article");
 $stmt->execute();
 
 echo "This printed\n";
